839f5bba0a34cb637e28a4e11e3992c8d12d592d
TaWeiTu <tu.da.wei@gmail.com>
2021-04-08 00:35:57 +0800

    Add register page

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ extern crate rocket;
 extern crate rocket_client_addr;
 
 use rocket::request::Form;
-use rocket::response::NamedFile;
+use rocket::response::{NamedFile, Redirect};
 use rocket_client_addr::ClientAddr;
 use std::io;
 
@@ -23,16 +23,34 @@ struct LoginInfo {
     password: String,
 }
 
-#[post("/login", data = "<user>")]
-fn login(user: Option<Form<LoginInfo>>, client_addr: ClientAddr) -> &'static str {
+#[get("/register")]
+fn register() -> io::Result<NamedFile> {
+    NamedFile::open("static/register.html")
+}
+
+#[post("/login?<is_register>", data = "<user>")]
+fn login(
+    user: Option<Form<LoginInfo>>,
+    is_register: Option<bool>,
+    client_addr: ClientAddr,
+) -> Redirect {
+    let is_register = is_register.unwrap_or_default();
     let addr = client_addr.get_ipv4().unwrap();
+    eprintln!("is_register = {} addr = {}", is_register, addr);
     match user {
         None => eprintln!("none"),
         Some(info) => eprintln!("name: {}, password: {}", info.username, info.password),
     }
-    "login"
+    Redirect::to("/success")
+}
+
+#[get("/success")]
+fn success() -> io::Result<NamedFile> {
+    NamedFile::open("static/success.html")
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![index, login]).launch();
+    rocket::ignite()
+        .mount("/", routes![index, login, register, success])
+        .launch();
 }
